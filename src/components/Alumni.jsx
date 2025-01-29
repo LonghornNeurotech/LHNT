9101cbbf348197dd8ea1614c8bbdeeb05177575b
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-01-28 19:23:18 -0600

    Created and provided a starter wireframe for the Alumni page!

---
+++ src/components/Alumni.jsx
@@ -0,0 +1,38 @@
+import SponsorWidget from './SponsorWidget';
+
+const Alumni = () => {
+    return (
+        <div 
+            style={{ 
+                fontFamily: 'Antonio',
+                backgroundColor: '#003153',
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column'
+            }}
+        >
+            <div style={{ paddingTop: '80px' }}>
+                <header 
+                    className="text-center py-8"
+                >
+                    <h1 className="text-[#F3E5AB] text-5xl mb-4 heading">Our Alumni</h1>
+                    <p className="text-[#F9F6EE] text-3xl subheading">Celebrating the achievements of our alumni</p>
+                </header>
+
+                <main className="px-8 py-6">
+                    <p className="text-[#F9F6EE] text-2xl baseText">
+                        Welcome to the Alumni page! Here we showcase the incredible journeys and accomplishments of our alumni.
+                    </p>
+                    {/* Alumni cards or list will go here */}
+                </main>
+            </div>
+
+            {/* Push SponsorWidget to bottom */}
+            <div className="mt-auto">
+                <SponsorWidget />
+            </div>
+        </div>
+    );
+};
+
+export default Alumni;
--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,8 @@ import Contact from "./Contact";
 import Home from "./Home";
 
 import Events from "./Events";
+import Alumni from "./Alumni";
 import SponsorWidget from "./SponsorWidget";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export { Navbar, About, Contact, Home, Events, SponsorWidget };
+export { Navbar, About, Contact, Home, Events, Alumni, SponsorWidget };
