cdf8f3924ad7ebbcf1d6334756c645b02d97f40d
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-01-28 20:13:39 -0600

    Set up the Alumni components so they can be used by other components outside the Alumni directory!

--- src/components/Alumni/alumni.js
+++ src/components/Alumni/alumni.js
@@ -4,4 +4,12 @@
     outside the Alumni page can import with a more simpler 
     import command by importing the specified components 
     from this file!
+
+    This is one example of import command I was talking about:
+    import { Profile, ProfileDisplay, Search, Filter } from './alumni';
 */
+
+export { default as Profile } from './Profile/Profile';
+export { default as ProfileDisplay } from './ProfileDisplay/ProfileDisplay';
+export { default as Search } from './Search/Search';
+export { default as Filter } from './Filter/Filter';
