5521cee0bb9c1c2ed7e9f1c41261476d8fecccaa
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-02-16 15:28:15 -0600

    Implemented rearrangement of content in alumni profile card if there is no image for alumni or the image fails to display!

--- src/components/Alumni/Profile/Profile.jsx
+++ src/components/Alumni/Profile/Profile.jsx
@@ -8,11 +8,23 @@
 const Profile = ({ name, photo, major, graduationYear, bio }) => {
   return (
     <div className="bg-[#FFF8D6] rounded-lg shadow-md p-4 flex flex-col items-center">
-      <img
-        src={photo || 'default-profile.jpg'}
-        alt={name}
-        className="w-32 h-32 rounded-full object-cover mb-2"
-      />
+      {photo && (
+        <img 
+          src={photo}
+          alt={name}
+          className="w-32 h-32 rounded-full object-cover mb-2"
+          /* 
+            If image fails to load or there are no image for the 
+            alumni, then responsively rearrange the other elements
+            of an alumni's Profile card as if the image doesn't
+            exist for the alumni's Profile card!
+          */
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.style.display = 'none'
+          }}
+        />
+      )}
       <h3 className="text-xl font-semibold text-prussian-blue break-words text-center">{name}</h3>
       <p className="text-sm text-prussian-blue">{major} - {graduationYear}</p>
       <p className="text-sm text-prussian-blue text-center mt-2">{bio}</p>
