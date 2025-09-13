12181ad5947ff242dd24b300aa2fe8c1f282ae36
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-09-13 10:12:03 -0500

    Created a new config directory to keep files with configuration settings in an easier place to go to. Also created quizMap.js file to provide unique ID values to access specific quiz data for a specific module in a specific onboarding block!

---
+++ src/config/quizMap.js
@@ -0,0 +1,7 @@
+// Map each JSON file in the quizzes directory to a specific quiz with a unique ID value
+const quizMap = {
+    hardware2Module1_1: '/data/quizzes/hardware2Module1_1.json',
+    hardware2Module1_3: '/data/quizzes/hardware2Module1_3.json',
+}
+
+export default quizMap;
