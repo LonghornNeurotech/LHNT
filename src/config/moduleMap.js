644bf18d6cbc0d44bdfb775f1cbcfb38317a32d5
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-09-18 21:19:13 -0500

    Updated the moduleMap.js file to provide info on grouping submodules based on associated modules and the titles of each onboarding block!

--- src/config/moduleMap.js
+++ src/config/moduleMap.js
@@ -1,35 +1,51 @@
 // moduleMap.js
-export const moduleMap = {
-  hardwareb2: {
+const moduleMap = {
+  hardwareB2: {
     title: "Hardware Block 2: Design",
-    groups: [
-      {
-        label: "Module 1",
-        id: "1",
-        submodules: [
-          { moduleId: "1_1", label: "Module 1.1" },
-          { moduleId: "1_2", label: "Module 1.2" },
-          { moduleId: "1_3", label: "Module 1.3" },
-        ]
+    groups: {
+      "Module 1": ["1_1", "1_2", "1_3"],
+      "Module 2": ["2_1", "2_2"],
+      "Module 3": ["3_1", "3_2", "3_3", "3_4"]
+    },
+    modules: {
+      "1_1": {
+        title: "Module 1.1: Advanced CAD",
+        path: "/onboarding/hardwareB2/1_1"
       },
-      {
-        label: "Module 2",
-        id: "2",
-        submodules: [
-          { moduleId: "2_1", label: "Module 2.1" },
-          { moduleId: "2_2", label: "Module 2.2" },
-        ]
+      "1_2": {
+        title: "Module 1.2: Blender",
+        path: "/onboarding/hardwareB2/1_2"
       },
-      {
-        label: "Module 3",
-        id: "3",
-        submodules: [
-          { moduleId: "3_1", label: "Module 3.1" },
-          { moduleId: "3_2", label: "Module 3.2" },
-          { moduleId: "3_3", label: "Module 3.3" },
-          { moduleId: "3_4", label: "Module 3.4" }
-        ]
+      "1_3": {
+        title: "Module 1.3: Simulation",
+        path: "/onboarding/hardwareB2/1_3"
+      },
+      "2_1": {
+        title: "Module 2.1: PCB Design",
+        path: "/onboarding/hardwareB2/2_1"
+      },
+      "2_2": {
+        title: "Module 2.2: Component Soldering",
+        path: "/onboarding/hardwareB2/2_2"
+      },
+      "3_1": {
+        title: "Module 3.1: Testing",
+        path: "/onboarding/hardwareB2/3_1"
+      },
+      "3_2": {
+        title: "Module 3.2: Debugging",
+        path: "/onboarding/hardwareB2/3_2"
+      },
+      "3_3": {
+        title: "Module 3.3: Documentation",
+        path: "/onboarding/hardwareB2/3_3"
+      },
+      "3_4": {
+        title: "Module 3.4: Final Assembly",
+        path: "/onboarding/hardwareB2/3_4"
       }
-    ]
+    }
   }
 };
+
+export default moduleMap;
