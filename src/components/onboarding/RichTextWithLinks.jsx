4817f3cd2bb08754fe947fb7d0b03d4a3d9d0540
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-09-23 15:13:05 -0500

    Correctly implemented progress tracking for all required links embedded in task description!

--- src/components/onboarding/RichTextWithLinks.jsx
+++ src/components/onboarding/RichTextWithLinks.jsx
@@ -1,9 +1,5 @@
 // ./src/components/onboarding/RichTextWithLinks.jsx
 // This is the clickable links embedded in the text content of the submodule page for user to click on.
-// ./src/components/onboarding/RichTextWithLinks.jsx
-
-// This is the clickable links embedded in the text content of the submodule page for user to click on.
-
 import PropTypes from "prop-types";
 import { useState } from "react";
 
@@ -79,76 +75,3 @@ RichTextWithLinks.propTypes = {
 };
 
 export default RichTextWithLinks;
-
-
-
-// import PropTypes from "prop-types";
-// import { useState } from "react";
-
-// const RichTextWithLinks = ({ text, links = [] }) => {
-//   const [clickedLinks, setClickedLinks] = useState({});
-
-//   const linkMap = Object.fromEntries(links.map((link) => [link.label, link]));
-
-//   const regex = /\[([^\]]+)]/g;
-//   const parts = [];
-//   let lastIndex = 0;
-//   let match;
-
-//   while ((match = regex.exec(text)) !== null) {
-//     const label = match[1];
-//     const link = linkMap[label];
-
-//     if (match.index > lastIndex) {
-//       parts.push(text.substring(lastIndex, match.index));
-//     }
-
-//     if (link) {
-//       const isClicked = clickedLinks[label] || false;
-//       const baseClass = link.required
-//         ? "text-orange-600 font-semibold underline"
-//         : "text-blue-600 underline";
-//       const clickedClass = link.required
-//         ? "text-green-800 font-semibold underline"
-//         : "text-blue-800 underline";
-
-//       parts.push(
-//         <a
-//           key={label + match.index}
-//           href={link.url}
-//           target="_blank"
-//           rel="noopener noreferrer"
-//           className={isClicked ? clickedClass : baseClass}
-//           onClick={() =>
-//             setClickedLinks((prev) => ({ ...prev, [label]: true }))
-//           }
-//         >
-//           {label}
-//         </a>
-//       );
-//     } else {
-//       parts.push(match[0]);
-//     }
-
-//     lastIndex = regex.lastIndex;
-//   }
-
-//   if (lastIndex < text.length) {
-//     parts.push(text.substring(lastIndex));
-//   }
-
-//   return <span>{parts}</span>;
-// };
-
-// RichTextWithLinks.propTypes = {
-//   text: PropTypes.string.isRequired,
-//   links: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       label: PropTypes.string.isRequired,
-//       url: PropTypes.string.isRequired,
-//       required: PropTypes.bool,
-//     })
-//   ),
-// };
-
-// export default RichTextWithLinks;
